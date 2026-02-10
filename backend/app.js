df8e44fbe1e8bd1523fc6a65de31995de3cd5b46
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-02-10 18:24:10 -0300

    CORS fixed

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',  
         'http://localhost:3000',  
-        'https://endehafront.onrender.com/'  
+        'https://endehafront.onrender.com'  
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
